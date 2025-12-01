b8508aabf544e64cf962e62937c8d6ba56fdf8df
prinsonnazareth2@gmail.com <prinsonnazareth2@gmail.com>
2025-12-01 23:04:29 +0530

    match and loop

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
-fn main() {
+fn main(){
+   let a = 6;
 
-   let day = 6;
+   match a {
+      1 => println!("monday"),
+      2 => println!("tuesday"),
+      3 => println!("wednesday"),
+      4 => println!("thursday"),
+      5 => println!("friday"),
+      6 => println!("saturday"),
+      7 => println!("sunday"),
+      _ => println!("its not a number")
+   }
 
-   match day {
-    1 => println!("monday"),
-    2 => println!("tuesday"),
-    3 => println!("wednesday"),
-    4 => println!("thursday"),
-    5 => println!("friday"),
-    6 => println!("saturday"),
-    7 => println!("sunday"),
-    _ =>  println!("invalid day"),
+   let mut count = 1;
+
+   loop{
+      println!("hello world");
+
+      if count == 3{
+         break;
+      }
+
+      count+= 1;
    }
 }
